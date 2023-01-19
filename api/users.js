b36bd161c9937ff16a56f7cf94b45dd7bb9ddf57
Natoshia Dumont <natoshia.dumont@gmail.com>
2023-01-18 22:44:35 -0600

    troubleshooting jwt authentication

--- api/users.js
+++ api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const usersRouter = express.Router();
-const {getAllUsers} = require('../db');
+const { getAllUsers, getUserByUsername } = require('../db');
+const jwt = require('jsonwebtoken');
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -14,6 +15,35 @@ usersRouter.get('/', async (req, res) => {
   res.send({
     users
   });
+});
+
+usersRouter.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password"
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      const token = jwt.sign({ username: username, password: password }, process.env.JWT_SECRET);
+      res.send({ message: "you're logged in!", token: token });
+    } else {
+      next({
+        name: 'IncorrectCredentialsError',
+        message: 'Username or password is incorrect'
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 });
 
 module.exports = usersRouter;
